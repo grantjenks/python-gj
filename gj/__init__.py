f8097d1a17e2bfdcac5055452133ac624ed5594d
Grant Jenks <contact@grantjenks.com>
2017-02-15 10:13:34 -0800

    Bump version to 0.1.0

--- gj/__init__.py
+++ gj/__init__.py
@@ -3,8 +3,8 @@
 from .packages import release, upload_docs
 
 __title__ = 'gj'
-__version__ = '0.0.9'
-__build__ = 0x000009
+__version__ = '0.1.0'
+__build__ = 0x000100
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Grant Jenks'
