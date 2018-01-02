a10597a30e53969fac3458f9045b24d0f54dc516
Grant Jenks <contact@grantjenks.com>
2018-01-02 11:03:56 -0800

    Bump version to 0.4.0

--- gj/__init__.py
+++ gj/__init__.py
@@ -4,8 +4,8 @@ from .packages import release, upload_docs
 from .utils import watch
 
 __title__ = 'gj'
-__version__ = '0.3.4'
-__build__ = 0x000304
+__version__ = '0.4.0'
+__build__ = 0x000400
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Grant Jenks'
